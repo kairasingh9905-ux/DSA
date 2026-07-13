2356479d5b1e0b67df334feaaadfa6b937bb9425
kairasingh9905 <kairasingh9905@gmail.com>
2026-07-13 10:02:51 +0530

    added floyd pattern 2 with characters

---
+++ PATTERNS/floyd2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, n;
+    cout << "Enter number of rows(n):";
+    cin >> n;
+    char ch = 'A';
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << ch << " ";
+            ch += 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
