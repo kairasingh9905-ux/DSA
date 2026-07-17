a17c3f151ff84a919cc58f5021bd61653e4b2e73
kairasingh9905 <kairasingh9905@gmail.com>
2026-07-17 10:57:34 +0530

    added swapping min and max in an array

---
+++ ARRAY/swap.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int arr[], int sz)
+{
+    int b = INT_MIN;
+    int max = 0;
+    for (int i = 0; i < sz; i++)
+    {
+        if (arr[i] > b)
+        {
+            b = arr[i];
+            max = i;
+        }
+    }
+    return max;
+}
+int min(int arr[], int sz)
+{
+    int a = INT_MAX;
+    int min = 0;
+    for (int i = 0; i < sz; i++)
+    {
+        if (arr[i] < a)
+        {
+            a = arr[i];
+            min = i;
+        }
+    }
+    return min;
+}
+void swap(int &max_arr, int &min_arr)
+{
+    int temp = max_arr;
+    max_arr = min_arr;
+    min_arr = temp;
+}
+int main()
+{
+    int arr[] = {4, 2, 7, 8, 5};
+    int sz = 5;
+    int max_arr = max(arr, sz);
+    int min_arr = min(arr, sz);
+    swap(arr[max_arr], arr[min_arr]);
+    cout << "SWAPPED ARRAY= ";
+    for (int i = 0; i < sz; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
