bd037da55304466cab1e4ff78681a7851d5a2a0d
kairasingh9905 <kairasingh9905@gmail.com>
2026-07-22 21:17:55 +0530

    added finding binary expansion even negative

---
+++ POW/binary_exp.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int binary(int n)
+{
+    int ans = 0, pow = 1;
+    while (n > 0)
+    {
+        int rem = n % 2;
+        ans = ans + rem * pow;
+        pow = pow * 10;
+        n = n / 2;
+    }
+    return ans;
+}
+int main()
+{
+    double n, x;
+
+    cout << " enter value of integer whose expansions needs to be calculated:";
+    cin >> x;
+    cout << "enter power: ";
+    cin >> n;
+    int bin = binary(n);
+    double ans = 1, pow = x;
+    if (n < 0)
+    {
+        x = 1 / x;
+        bin = bin;
+    }
+    while (bin > 0)
+    {
+        if (bin % 10 == 1)
+        {
+            ans = x * pow;
+            pow = pow * pow;
+        }
+        else
+        {
+            pow = pow * pow;
+        }
+        bin = bin / 10;
+    }
+    cout << ans << endl;
+    return 0;
+}
