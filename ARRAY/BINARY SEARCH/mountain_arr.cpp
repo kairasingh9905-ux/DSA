1ead0d66632d8423ad6f99e4768004497dd0c184
kairasingh9905 <kairasingh9905@gmail.com>
2026-08-01 10:03:07 +0530

    added peak index index in mountain array

---
+++ ARRAY/BINARY SEARCH/mountain_arr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int search(vector<int> arr)
+{
+    int st = 0, end = arr.size() - 1;
+    int mid = st + (end - st) / 2;
+    while (st < end)
+    {
+        int mid = st + (end - st) / 2;
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) // peak value check
+        {
+            return mid;
+        }
+
+        if (arr[mid - 1] < arr[mid]) // mid lies in left side (inc) right search
+        {
+            st = mid + 1;
+        }
+        else // mid lies in right side(dec) left search
+        {
+            end = mid - 1;
+        }
+    }
+    return st; // shrinking condition
+}
+int main()
+{
+    vector<int> arr = {0, 3, 8, 9, 5, 2};
+    cout << "PEAK INDEX IN MOUNTAIN ARRAY: " << search(arr) << endl;
+}
