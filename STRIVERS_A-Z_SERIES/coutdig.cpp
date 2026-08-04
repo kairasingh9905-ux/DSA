effdc57bf9b026ebbdd35d953ee882cf5fa49f15
kairasingh9905 <kairasingh9905@gmail.com>
2026-08-04 12:32:25 +0530

    added count digits of a number

---
+++ STRIVERS_A-Z_SERIES/coutdig.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int count_dig(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int dig = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        dig += 1;
+    }
+    return dig;
+}
+int main()
+{
+    int n;
+    cout << "enter number:";
+    cin >> n;
+    cout << count_dig(n) << endl;
+}
