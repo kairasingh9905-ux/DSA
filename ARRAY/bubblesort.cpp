47a021f41fc1e9be441d846cfe45568f54b0be51
kairasingh9905 <kairasingh9905@gmail.com>
2026-08-17 19:32:55 +0530

    added boubble sorting

---
+++ ARRAY/bubblesort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void bubble(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+void printarr(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    vector<int> arr = {4, 1, 5, 2, 3};
+    int n = arr.size();
+    bubble(arr);
+    printarr(arr, n);
+}
