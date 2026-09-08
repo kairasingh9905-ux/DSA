11fb87bc4304f722d4f44cea0049224f024fa3b3
kairasingh9905 <kairasingh9905@gmail.com>
2026-09-08 23:12:51 +0530

    added comments

--- STRINGS/reverse_sen.cpp
+++ STRINGS/reverse_sen.cpp
@@ -5,19 +5,19 @@ string reverse(string s)
 {
     int n = s.length();
     string ans = "";
-    reverse(s.begin(), s.end());
+    reverse(s.begin(), s.end()); // reversing whole sentence
     for (int i = 0; i < n; i++)
     {
         string word = "";
-        while (i < n && s[i] != ' ')
+        while (i < n && s[i] != ' ') // travesring through word
         {
             word += s[i];
             i++;
         }
-        reverse(word.begin(), word.end());
+        reverse(word.begin(), word.end()); // reversing only word to get reversed sentence
         ans += " " + word;
     }
-    return ans.substr(1);
+    return ans.substr(1); // removes fist whitespace from sentence
 }
 int main()
 {
