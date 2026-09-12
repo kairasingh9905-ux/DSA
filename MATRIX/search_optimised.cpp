9a9298be4198ddfe621f0fce0727dfd932e52be2
kairasingh9905 <kairasingh9905@gmail.com>
2026-09-12 10:48:55 +0530

    added comments

--- MATRIX/search_optimised.cpp
+++ MATRIX/search_optimised.cpp
@@ -8,15 +8,15 @@ bool indx(vector<vector<int>> &mat, int tar, int row)
     while (st <= end)
     {
         int mid = st + (end - st) / 2;
-        if (mat[row][mid] == tar)
+        if (mat[row][mid] == tar) // target found
         {
             return true;
         }
-        else if (tar > mat[mid][row])
+        else if (tar > mat[mid][row]) // target is greater than mid
         {
             st = mid + 1;
         }
-        else
+        else // target is lesser than mid
         {
             end = mid - 1;
         }
@@ -25,21 +25,21 @@ bool indx(vector<vector<int>> &mat, int tar, int row)
 }
 bool search(vector<vector<int>> &mat, int tar)
 {
-    int m = mat.size(), n = mat[0].size();
+    int m = mat.size(), n = mat[0].size(); // m= matrix size n= size of the row
     int st = 0, end = m - 1;
     while (st <= end)
     {
         int mid = st + (end - st) / 2;
         {
-            if (mat[mid][0] <= tar && mat[mid][n - 1] >= tar)
+            if (mat[mid][0] <= tar && mat[mid][n - 1] >= tar) // row found
             {
-                return indx(mat, tar, mid);
+                return indx(mat, tar, mid); // for binary search in a a row
             }
-            else if (mat[mid][n - 1] <= tar)
+            else if (mat[mid][n - 1] <= tar) // if target is below mid row
             {
                 st = mid + 1;
             }
-            else
+            else // if target is above mid row
             {
                 end = mid - 1;
             }
