c29c0fa9906d231986df0f62895218fd9f076853
kairasingh9905 <kairasingh9905@gmail.com>
2026-09-24 18:01:54 +0530

    added comments

--- MATRIX/spiral.cpp
+++ MATRIX/spiral.cpp
@@ -5,19 +5,19 @@ void spiral(int mat[][4], int rows, int cols)
     int strow = 0, stcol = 0, endrow = cols - 1, endcols = rows - 1;
     while (strow <= endrow && stcol <= endcols)
     {
-        for (int i = stcol; i <= endcols; i++)
+        for (int i = stcol; i <= endcols; i++) // for top
         {
             cout << mat[strow][i] << " ";
         }
-        for (int i = strow + 1; i <= endrow; i++)
+        for (int i = strow + 1; i <= endrow; i++) // for right
         {
             cout << mat[i][endcols] << " ";
         }
-        for (int i = endcols - 1; i >= stcol; i--)
+        for (int i = endcols - 1; i >= stcol; i--) // for bottom
         {
             cout << mat[endrow][i] << " ";
         }
-        for (int i = endrow - 1; i >= strow + 1; i--)
+        for (int i = endrow - 1; i >= strow + 1; i--) // for left
         {
             cout << mat[i][stcol] << " ";
         }
